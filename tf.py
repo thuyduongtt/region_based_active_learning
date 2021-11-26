eaf24019bf06eeacdb171f1beea4e12214b94c31
Duong <mia@dttt.io>
2021-11-26 11:25:47 +0100

    * Make it work

--- tf.py
+++ tf.py
@@ -5,3 +5,6 @@ if __name__ == '__main__':
     list_devices()
     gpu_available = tf.test.is_gpu_available()
     print('GPU available:', gpu_available)
+
+    with tf.Session(config=tf.ConfigProto(log_device_placement=True)).as_default() as sess:
+        print('Session has started !')
