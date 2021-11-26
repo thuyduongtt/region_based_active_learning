96a54e4aedba0af878cf184db6d680391e331e69
Duong <mia@dttt.io>
2021-11-26 11:06:31 +0100

    * Make it work

--- slurm.sh
+++ slurm.sh
@@ -3,19 +3,19 @@
 #SBATCH -J regional_active_learning    # Job Name
 
 ## Leave these values as they are unless you know what you are doing
-# SBATCH --ntasks=1             # Tasks
-# SBATCH --nodes=1              # Number of nodes
-# SBATCH --ntasks-per-core=2    # Number of processes per CPU code
-# SBATCH --cpus-per-task=1      # Number of CPUs per task?
+#SBATCH --ntasks=1             # Tasks
+#SBATCH --nodes=1              # Number of nodes
+#SBATCH --ntasks-per-core=2    # Number of processes per CPU code
+#SBATCH --cpus-per-task=1      # Number of CPUs per task?
 
 ## Adjust to your needs
-# SBATCH --mem=25GB             # Memory limit per node
-# SBATCH --time=168:00:00        # Expected maximum run time
-# SBATCH --partition=standard   # This is for testing on CPU
+#SBATCH --mem=25GB             # Memory limit per node
+#SBATCH --time=168:00:00        # Expected maximum run time
+#SBATCH --partition=standard   # This is for testing on CPU
 
 ## Job-Status per Mail
-# SBATCH --mail-type=ALL
-# SBATCH --mail-user=t.tranthi@campus.tu-berlin.de
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=t.tranthi@campus.tu-berlin.de
 
 ulimit -u 512
 rm -d output/ -r
--- slurm_gpu.sh
+++ slurm_gpu.sh
@@ -3,20 +3,20 @@
 #SBATCH -J regional_active_learning    # Job Name
 
 ## Leave these values as they are unless you know what you are doing
-# SBATCH --ntasks=1             # Tasks
-# SBATCH --nodes=1              # Number of nodes
-# SBATCH --ntasks-per-core=2    # Number of processes per CPU code
-# SBATCH --cpus-per-task=1      # Number of CPUs per task?
-# SBATCH --gres=gpu:tesla:1     # Use 1 GPU per node
+#SBATCH --ntasks=1             # Tasks
+#SBATCH --nodes=1              # Number of nodes
+#SBATCH --ntasks-per-core=2    # Number of processes per CPU code
+#SBATCH --cpus-per-task=1      # Number of CPUs per task?
+#SBATCH --gres=gpu:tesla:1     # Use 1 GPU per node
 
 ## Adjust to your needs
-# SBATCH --mem=25GB             # Memory limit per node
-# SBATCH --time=50:00:00        # Expected maximum run time
-# SBATCH --partition=gpu        # This is needed to use a GPU
+#SBATCH --mem=25GB             # Memory limit per node
+#SBATCH --time=50:00:00        # Expected maximum run time
+#SBATCH --partition=gpu        # This is needed to use a GPU
 
 ## Job-Status per Mail
-# SBATCH --mail-type=ALL
-# SBATCH --mail-user=t.tranthi@campus.tu-berlin.de
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=t.tranthi@campus.tu-berlin.de
 
 ulimit -u 512
 rm -d output/ -r
